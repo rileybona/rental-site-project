68a10552a2d2adbc86f6c1081ff41cd5b20a2a05
Riley Bona <rileybona7@gmail.com>
2024-03-28 15:43:49 -0400

    add users table migration and seeds

---
+++ backend/db/seeders/20240328175905-demo-user.js
@@ -0,0 +1,48 @@
+'use strict';
+
+const { User } = require('../models');
+const bcrypt = require('bcryptjs');
+
+let options = {};
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;
+}
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await User.bulkCreate([
+      {
+        email: 'demo@user.com',
+        username: 'Demo-lition',
+        hashedPassword: bcrypt.hashSync('password')
+      },
+      {
+        email: 'user123@urmom.com',
+        username: 'FakeUser123',
+        hashedPassword: bcrypt.hashSync('password2')
+      },
+      {
+        email: 'userthethird@testing.edu',
+        username: 'FakeUser3',
+        hashedPassword: bcrypt.hashSync('password3')
+      }
+    ], { validate: true });
+
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+    options.tableName = 'Users';
+    const Op = Sequelize.Op;
+    return awaitqueryInterface.bulkDelete(options, {
+      username: { [Op.in]: ['Demo-lition', 'FakeUser123', 'FakeUser3'] }
+    }, {});
+  }
+
+};
