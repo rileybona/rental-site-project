2fff15ee51eb494cfc2974d103cf6cbe88a93753
Riley Bona <rileybona7@gmail.com>
2024-03-29 19:09:12 -0400

    updated seed files to include first/last

--- backend/db/seeders/20240328175905-demo-user.js
+++ backend/db/seeders/20240328175905-demo-user.js
@@ -15,16 +15,22 @@ module.exports = {
       {
         email: 'demo@user.com',
         username: 'Demo-lition',
+        firstName: 'Demo',
+        lastName: 'Usenhouse',
         hashedPassword: bcrypt.hashSync('password')
       },
       {
         email: 'user123@urmom.com',
         username: 'FakeUser123',
+        firstName: 'Frank',
+        lasName: "Gallegher",
         hashedPassword: bcrypt.hashSync('password2')
       },
       {
         email: 'userthethird@testing.edu',
         username: 'FakeUser3',
+        firsName: 'Josie',
+        lastName: 'Pink',
         hashedPassword: bcrypt.hashSync('password3')
       }
     ], { validate: true });
