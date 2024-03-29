e23eb4d9d97c0991e512f3dc4d556087d9e15145
Riley Bona <rileybona7@gmail.com>
2024-03-29 12:42:00 -0400

    Add a backend endpoint to get the current user session

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -43,6 +43,30 @@ router.post('/', async (req, res, next) => {
 
 });
 
+
+// backend/routes/api/session.js
+// ...
+
+// Restore session user
+router.get(
+    '/',
+    (req, res) => {
+      const { user } = req;
+      if (user) {
+        const safeUser = {
+          id: user.id,
+          email: user.email,
+          username: user.username,
+        };
+        return res.json({
+          user: safeUser
+        });
+      } else return res.json({ user: null });
+    }
+  );
+
+  // ...
+
 // LOG OUT babay
 router.delete(
     '/',
