70e548235e0995d83b3ddb64fcc51351fdffdda3
Riley Bona <rileybona7@gmail.com>
2024-04-01 13:05:03 -0400

    Adjusted to one migration for server debuggin

--- backend/db/migrations/20240328174206-create-user.js
+++ backend/db/migrations/20240328174206-create-user.js
@@ -29,6 +29,14 @@ module.exports = {
         type: Sequelize.STRING,
         allowNull: false
       },
+      firstName: {
+        type: Sequelize.STRING(100),
+        allowNull: false,
+      },
+      lastName: {
+        type: Sequelize.STRING(100),
+        allowNull: false
+      },
       createdAt: {
         allowNull: false,
         type: Sequelize.DATE,
