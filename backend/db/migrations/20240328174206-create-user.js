f64d4c98752060d22a09e1ef7e79aca24b26ef07
Riley Bona <rileybona7@gmail.com>
2024-04-01 13:31:30 -0400

    yay! more option object debugging!

--- backend/db/migrations/20240328174206-create-user.js
+++ backend/db/migrations/20240328174206-create-user.js
@@ -50,7 +50,7 @@ module.exports = {
     });
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable('Users');
+    options.tableName = 'Users';
     return queryInterface.dropTable(options);
   }
 };
--- backend/db/seeders/20240328175905-demo-user.js
+++ backend/db/seeders/20240328175905-demo-user.js
@@ -38,12 +38,6 @@ module.exports = {
   },
 
   async down (queryInterface, Sequelize) {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
     options.tableName = 'Users';
     const Op = Sequelize.Op;
     return await queryInterface.bulkDelete(options, {
