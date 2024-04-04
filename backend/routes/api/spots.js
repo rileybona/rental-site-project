e6de78cd0b139ee2499e866c9e25bb99c8a3315c
Riley Bona <rileybona7@gmail.com>
2024-04-03 20:14:38 -0400

    Add preview image to get-all-spots response

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -2,7 +2,8 @@ const express = require('express');
 const router = express.Router();
 
 const { Op } = require('sequelize');
-
+const { Review } = require('../../db/models');
+const { SpotImage } = require('../../db/models');
 
 const { Spot } = require('../../db/models');
 
@@ -10,12 +11,17 @@ const { Spot } = require('../../db/models');
 // GET ALL SPOTS
 router.get('/', async (req, res) => {
     const spots = await Spot.findAll({
-        include: 'Reviews'
+        include: [{
+            model: Review,
+        }, {
+            model: SpotImage,
+        }],
     });
 
-    console.log(spots);
+    
 
     spots.forEach((Spot) => {
+        // create avgRating attribute for each spot
         let revArray = Spot.dataValues.Reviews;
         delete Spot.dataValues.Reviews;
         if (revArray.length === 0) {
@@ -26,6 +32,21 @@ router.get('/', async (req, res) => {
             let avg = ( sum / revArray.length);
             Spot.dataValues.avgRating = avg;
         }
+
+        // create previewImage attribute for each spot
+        let imgArray = Spot.dataValues.SpotImages;
+        delete Spot.dataValues.SpotImages;
+        console.log(imgArray);
+        // set a default image
+        Spot.dataValues.previewImage = 'https://images.pexels.com/photos/101808/pexels-photo-101808.jpeg?auto=compress&cs=tinysrgb&w=800';
+
+        // iterate through images, if one is preview, set preview to that image
+        imgArray.forEach((SpotImage) => {
+            if (SpotImage.dataValues.preview === true) {
+                console.log("its happening, folks!");
+                Spot.dataValues.previewImage = SpotImage.dataValues.url;
+            }
+        });
     });
 
     res.json(spots);
