09f4ba10ec76b929ecc8ee6fb757422944fd3e10
Riley Bona <rileybona7@gmail.com>
2024-04-04 16:14:23 -0400

    Add get spots owned by current user route

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -10,6 +10,38 @@ const { Spot } = require('../../db/models');
 
 const { handleValidationErrors } = require('../../utils/validation');
 
+// helper func to find avg rating
+function setAvgStars(Spot) {
+    // create avgRating attribute for each spot
+    let revArray = Spot.dataValues.Reviews;
+    delete Spot.dataValues.Reviews;
+    if (revArray.length === 0) {
+        Spot.dataValues.avgRating = "This spot has no reviews yet.";
+    } else {
+        let sum = 0;
+        revArray.forEach((rev) => { sum += rev.stars });
+        let avg = ( sum / revArray.length);
+        Spot.dataValues.avgRating = avg;
+    }
+};
+// helper func to find preview image
+function setPreviewImage(Spot) {
+    let imgArray = Spot.dataValues.SpotImages;
+    delete Spot.dataValues.SpotImages;
+    // console.log(imgArray);
+    // set a default image
+    Spot.dataValues.previewImage = 'https://images.pexels.com/photos/101808/pexels-photo-101808.jpeg?auto=compress&cs=tinysrgb&w=800';
+
+    // iterate through images, if one is preview, set preview to that image
+    imgArray.forEach((SpotImage) => {
+        if (SpotImage.dataValues.preview === true) {
+            // console.log("its happening, folks!");
+            Spot.dataValues.previewImage = SpotImage.dataValues.url;
+        }
+    });
+};
+
+
 // GET ALL SPOTS
 
 router.get('/', async (req, res) => {
@@ -21,40 +53,52 @@ router.get('/', async (req, res) => {
         }],
     });
 
-
-
     spots.forEach((Spot) => {
         // create avgRating attribute for each spot
-        let revArray = Spot.dataValues.Reviews;
-        delete Spot.dataValues.Reviews;
-        if (revArray.length === 0) {
-            Spot.dataValues.avgRating = "This spot has no reviews yet.";
-        } else {
-            let sum = 0;
-            revArray.forEach((rev) => { sum += rev.stars });
-            let avg = ( sum / revArray.length);
-            Spot.dataValues.avgRating = avg;
-        }
+        setAvgStars(Spot);
 
         // create previewImage attribute for each spot
-        let imgArray = Spot.dataValues.SpotImages;
-        delete Spot.dataValues.SpotImages;
-        // console.log(imgArray);
-        // set a default image
-        Spot.dataValues.previewImage = 'https://images.pexels.com/photos/101808/pexels-photo-101808.jpeg?auto=compress&cs=tinysrgb&w=800';
-
-        // iterate through images, if one is preview, set preview to that image
-        imgArray.forEach((SpotImage) => {
-            if (SpotImage.dataValues.preview === true) {
-                // console.log("its happening, folks!");
-                Spot.dataValues.previewImage = SpotImage.dataValues.url;
-            }
-        });
+        setPreviewImage(Spot);
     });
 
     res.json(spots);
 });
 
+
+// GET SPOTS BY CURRENT USER
+
+router.get('/current', requireAuth, async (req, res) => {
+    // get current user
+    const { user } = req;
+    const thisOwnerId = user.id;
+
+
+    // get spots & associate reviews & associated images
+    const spots = await Spot.findAll({
+        where: {
+            ownerId: thisOwnerId
+        },
+        include: [{
+            model: Review,
+        }, {
+            model: SpotImage,
+        }],
+    });
+
+    // if there are no spots under this user, return an appropriate response
+    if (spots.length === 0) {
+        return res.json({ "message": "You have not posted any spots yet !" });
+    };
+
+    // otherwise, set the avg rating and preview image of each spot
+    spots.forEach((Spot) => {
+        setAvgStars(Spot);
+        setPreviewImage(Spot);
+    });
+
+    // return the user's spots ! 
+    res.json(spots);
+})
 
 
 // CREATE A SPOT
