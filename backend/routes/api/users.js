08dc05d1aed5b95bf0d3191f515e8e5bba4cde76
Riley Bona <rileybona7@gmail.com>
2024-04-05 19:19:33 -0400

    Addressed Pre-grade Notes

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -14,6 +14,12 @@ const validateSignup = [
       .exists({ checkFalsy: true })
       .isEmail()
       .withMessage('Please provide a valid email.'),
+    check('firstName')
+      .exists({ checkFalsy: true})
+      .withMessage('First Name is required'),
+    check('lastName')
+      .exists({ checkFalsy: true})
+      .withMessage('Last Name is required'),
     check('username')
       .exists({ checkFalsy: true })
       .isLength({ min: 4 })
